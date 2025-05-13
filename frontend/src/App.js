814753ee9ae90c1e970a5911823b517d247c053c
darlingdiamond01 <darlingmaria01@gmail.com>
2025-05-13 14:03:05 +0100

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/products/')
+      .then(response => {
+        setProducts(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Fashion Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - ${product.price}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
